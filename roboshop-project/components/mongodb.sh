6b2e3277b5fcfb99cb3802eab0113ce58f80eda1
Sri <srilakshmi.vippala@gmail.com>
2022-07-28 15:05:35 -0400

    Installing MongoDB

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -7,3 +7,21 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 echo "Install MongoDB"
 yum install -y mongodb.org &>>LOG_FILE
 
+echo "Update MongoDB Config File"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+
+echo "Start Database"
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
+
+echo "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+echo "Extract Schema"
+cd /tmp/
+unzip -o mongodb.zip &>>$LOG_FILE
+
+echo "Load Schema"
+cd mongodb-main
+mongo < catalogue.js &>>$LOG_FILE
+mongo < users.js &>>LOG_FILE
