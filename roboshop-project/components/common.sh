fcaf3546ad900a7c000002f1ee192dade0a99217
Sri <srilakshmi.vippala@gmail.com>
2022-08-01 14:42:21 -0400

    installing cart

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -58,8 +58,7 @@ chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
 stat $?
 
 echo "Update ${component} Systemd file"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${component}/systemd.service &>>$LOG_FILE
-stat $?
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/RABBITMQ-IP/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILEstat $?
 
 echo "setup ${component} SystemD  file"
  mv /home/roboshop/${component}/systemd.service  /etc/systemd/system/${components}.service &>>$LOG_FILE
